ee5894dbbcdcdd7ed7c19a8915c63133fb1056ed
stockpy <shakey001@hanmail.net>
2021-04-22 22:50:05 +0900

    Scheduled Commit

--- cmd_git.bat
+++ cmd_git.bat
@@ -3,6 +3,7 @@ cd python
 
 REM "C:\Program Files\Git\cmd\git.exe" config --global credential.helper store
 
+
 "C:\Program Files\Git\cmd\git.exe" status
 "C:\Program Files\Git\cmd\git.exe" add .
 "C:\Program Files\Git\cmd\git.exe" commit -m "Scheduled Commit"
@@ -22,3 +23,7 @@ REM "C:\Program Files\Git\cmd\git.exe" remote add origin git@github.com:stockpy/
 
 REM OpenSSH-Win32 로 한방에 됐음
 REM ssh-keygen -t rsa -b 4096 -C "stockpython"
+
+REM "C:\Program Files\Git\cmd\git.exe" pull origin master
+
+git add/rm <file>

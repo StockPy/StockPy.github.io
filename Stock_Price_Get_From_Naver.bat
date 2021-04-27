73d012872482aac87674a06bc341d74aba997925
stockpy <shakey001@hanmail.net>
2021-04-28 06:51:23 +0900

    Scheduled Commit

---
+++ Stock_Price_Get_From_Naver.bat
@@ -0,0 +1,10 @@
+REM 
+REM 
+REM (익스플로러) Pandas STOCK 시작 300초(5분) + 600초(10분) + 300초 (5분)
+REM 
+REM
+
+d:
+cd python
+
+start /b "C:\Program Files\Internet Explorer\iexplore.exe" http://192.168.10.51:8889/notebooks/Stock_Price_Get_From_Naver.ipynb
